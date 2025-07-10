4eb7c764898fadb69af53ffe2d59e0db81f95f9e
willgerman <williamgerman44@gmail.com>
2025-07-10 15:42:29 -0400

    updated nps router

--- src/routers/nps.js
+++ src/routers/nps.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { auth } from "../middleware/auth.js";
+import { default as utils } from "url"; // maybe not the best naming convention here ¯ \_(ツ)_/¯  but it conflicted with my pre-existing 'url' variable(s).
 
 const NPS_API_URL = process.env.NPS_API_URL;
 const NPS_API_KEY = process.env.NPS_API_KEY;
@@ -21,13 +22,18 @@ router.get('/national-parks', auth, async (req, res) => {
         let url = `${NPS_API_URL}/${endpoint}`;
 
         let options = {};
-        let query = "";
+        let query = ``;
 
-        if (!req.query.limit) {
-            query += `$limit=1`;
+        if (req.query && Object.keys(req.query).length > 0) {
+            query += `?${utils.parse(req.url, true).search}`;
+
+            if (!req.query.limit) {
+                query += `&limit=1`;
+            }
+        } else {
+            query += `?limit=1`;
         }
 
-        query = req._parsedUrl.search;
         query += `&api_key=${NPS_API_KEY}`;
 
         if (query) {
@@ -48,6 +54,11 @@ router.get('/national-parks', auth, async (req, res) => {
 
             // NOTE: Returns a 500 status to the client because the server exclusive NPS API key is invalid and there is nothing the client can do to fix this.
             if (response.status === 401) {
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
+
+            // NOTE: Returns a 500 status to the client because the server is sending a malformed query to the NPS API and there is nothing the client can do to fix this.
+            if (response.status === 403) {
                 return res.status(500).send("Server encountered an unexpected error. Please try again.");
             }
         }
@@ -123,6 +134,11 @@ router.get('/national-parks/codes', auth, async (req, res) => {
             if (response.status === 401) {
                 return res.status(500).send("Server encountered an unexpected error. Please try again.");
             }
+
+            // NOTE: Returns a 500 status to the client because the server is sending a malformed query to the NPS API and there is nothing the client can do to fix this.
+            if (response.status === 403) {
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
         }
     } catch (error) {
         console.log(error);
@@ -141,13 +157,77 @@ router.get('/campgrounds', auth, async (req, res) => {
         let url = `${NPS_API_URL}/${endpoint}`;
 
         let options = {};
-        let query = "";
+        let query = ``;
 
-        if (!req.query.limit) {
-            query += `$limit=1`;
+        if (req.query && Object.keys(req.query).length > 0) {
+            query += `?${utils.parse(req.url, true).search}`;
+
+            if (!req.query.limit) {
+                query += `&limit=1`;
+            }
+        } else {
+            query += `?limit=1`;
         }
 
-        query = req._parsedUrl.search;
+        query += `&api_key=${NPS_API_KEY}`;
+
+        if (query) {
+            url += query;
+        }
+
+        let response = await fetch(url, options);
+
+        if (response.ok) {
+            if (response.status === 200) {
+                const data = await response.json();
+                return res.status(200).send(data);
+            }
+        } else {
+            if (response.status === 400) {
+                console.log(response);
+                return res.status(400).send("Unable to retrieve the requested resource.");
+            }
+
+            // NOTE: Returns a 500 status to the client because the server exclusive NPS API key is invalid and there is nothing the client can do to fix this.
+            if (response.status === 401) {
+                console.log(response);
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
+
+            // NOTE: Returns a 500 status to the client because the server is sending a malformed query to the NPS API and there is nothing the client can do to fix this.
+            if (response.status === 403) {
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Server encountered an unexpected error. Please try again.");
+    }
+});
+
+/**
+ *  Get Activities
+ *  [docs link]
+ *  https://www.nps.gov/subjects/developer/api-documentation.htm#/activities/getActivities
+ */
+router.get('/activities', auth, async (req, res) => {
+    try {
+        const endpoint = 'activities';
+        let url = `${NPS_API_URL}/${endpoint}`;
+
+        let options = {};
+        let query = ``;
+
+        if (req.query && Object.keys(req.query).length > 0) {
+            query += `?${utils.parse(req.url, true).search}`;
+
+            if (!req.query.limit) {
+                query += `&limit=1`;
+            }
+        } else {
+            query += `?limit=1`;
+        }
+
         query += `&api_key=${NPS_API_KEY}`;
 
         if (query) {
@@ -170,6 +250,11 @@ router.get('/campgrounds', auth, async (req, res) => {
             if (response.status === 401) {
                 return res.status(500).send("Server encountered an unexpected error. Please try again.");
             }
+
+            // NOTE: Returns a 500 status to the client because the server is sending a malformed query to the NPS API and there is nothing the client can do to fix this.
+            if (response.status === 403) {
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
         }
     } catch (error) {
         console.log(error);
@@ -188,13 +273,18 @@ router.get('/things-to-do', auth, async (req, res) => {
         let url = `${NPS_API_URL}/${endpoint}`;
 
         let options = {};
-        let query = "";
+        let query = ``;
 
-        if (!req.query.limit) {
-            query += `$limit=1`;
+        if (req.query && Object.keys(req.query).length > 0) {
+            query += `?${utils.parse(req.url, true).search}`;
+
+            if (!req.query.limit) {
+                query += `&limit=1`;
+            }
+        } else {
+            query += `?limit=1`;
         }
 
-        query = req._parsedUrl.search;
         query += `&api_key=${NPS_API_KEY}`;
 
         if (query) {
@@ -215,6 +305,11 @@ router.get('/things-to-do', auth, async (req, res) => {
 
             // NOTE: Returns a 500 status to the client because the server exclusive NPS API key is invalid and there is nothing the client can do to fix this.
             if (response.status === 401) {
+                return res.status(500).send("Server encountered an unexpected error. Please try again.");
+            }
+
+            // NOTE: Returns a 500 status to the client because the server is sending a malformed query to the NPS API and there is nothing the client can do to fix this.
+            if (response.status === 403) {
                 return res.status(500).send("Server encountered an unexpected error. Please try again.");
             }
         }
--- src/routers/users.js
+++ src/routers/users.js
@@ -93,7 +93,7 @@ router.get('/users', auth, async (req, res) => {
         }
 
         const users = await User.find(
-            { $match: filter },
+            filter,
             {
                 userName: 1,
                 firstName: 1,
